83c887b4c7d001d45c7a118abb6af17af5f5d4b2
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-18 06:37:45 +0500

    Building /api/whoami Endpoint

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ app.get('/', (req, res) => {
   res.send('Header Parser Microservice');
 });
 
+app.get('/api/whoami', (req, res)=>{
+    const ipaddress = req.ip
+    const language = req.headers['accept-language']
+    const software = req.headers['user-agent']
+
+    console.log(req.headers);
+    
+    res.json({
+        ipaddress,
+        language,
+        software
+    })
+})
+
 app.get('/api/headers', (req, res) => {
   res.json(req.headers);
 });
